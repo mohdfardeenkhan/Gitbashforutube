a50b6afef97f8db68773ea939e1fadbd9feca33a
mohdfardeenkhan <bittufardeen@gmail.com>
2024-06-05 14:17:24 +0530

    type anything

---
+++ src/firstpackage/Sum.java
@@ -0,0 +1,27 @@
+package firstpackage;
+
+import java.util.Scanner;
+
+public class Sum
+{
+	public static void main (String[]args)
+	{
+		String str = "Hello world";
+		//System.out.println(num+"*"+i+"="+num*i);
+		System.out.println(str);
+		System.out.println(str.length());
+			
+		
+		for(int i=0; i<str.length();i++) {
+			System.out.println(str.charAt(i));
+		}
+		
+		System.out.println(str.toLowerCase());
+		System.out.println(str.toUpperCase());
+		System.out.println(str.indexOf("e"));
+	
+	}			
+}				
+				
+				
+				
